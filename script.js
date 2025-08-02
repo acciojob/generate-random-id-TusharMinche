e99155330d5778c198404d56a9283ee5ba191252
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-02 07:18:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+
+	let char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let ans = "";
+	for(let i = 0; i < l; i++){
+		ans += char_list[Math.floor(Math.random()*char_list.length)];
+	}
+
+	return ans;
 }
 
 // Do not change the code below.
